59cbbe2cb7b156aed7742cb1a2eedb12371818c3
rizkiariffudin <rizki.ariff3@gmail.com>
2023-09-10 21:08:29 +0700

    Inisiasi awal

---
+++ toko_anisa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for toko_anisa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toko_anisa.settings')
+
+application = get_asgi_application()
