516148ee81a04575d6a07c4bf4885fdfbd16e1f8
rizkiariffudin <rizki.ariff3@gmail.com>
2023-09-11 07:19:10 +0700

    Menambahkan testing sendiri

--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
-class Product(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
-    date_added = models.DateField(auto_now_add=True)
-    price = models.IntegerField()
+    amount = models.IntegerField()
     description = models.TextField()
--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,27 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_create_instance(self):
+        # Create an instance of Item
+        my_model = Item(name="Roti", amount=10, description="Roti bakar enak")
+
+        # Verify that the attributes match what you set
+        self.assertEqual(my_model.name, "Roti")
+        self.assertEqual(my_model.amount, 10)
+        self.assertEqual(my_model.description, "Roti bakar enak")
+
+    def test_save_and_retrieve(self):
+        # Create an instance of Item
+        my_model = Item(name="Pisang", amount=15, description="Pisang coklat")
+
+        # Save it to the database
+        my_model.save()
+
+        # Retrieve the object from the database
+        retrieved_model = Item.objects.get(name="Pisang")
+
+        # Verify that the retrieved object matches the saved object
+        self.assertEqual(retrieved_model.name, "Pisang")
+        self.assertEqual(retrieved_model.amount, 15)
+        self.assertEqual(retrieved_model.description, "Pisang coklat")
