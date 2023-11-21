859a241a1202cae59e842361fbb8a94150797830
rizkiariffudin <rizki.ariff3@gmail.com>
2023-11-21 07:55:56 +0700

    Menambahkan requirements untuk tugas 9

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, create_item_flutter
 from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
 from main.views import logout_user, add_amount, reduce_amount, remove_item
@@ -20,8 +20,9 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('add_amount/<int:item_id>/', add_amount, name='add_amount'),
     path('reduce_amount/<int:item_id>/', reduce_amount, name='reduce_amount'),
-    path('remove_item/<int:item_id>/', remove_item, name='remove_item'),
     path('edit-item/<int:id>', edit_item, name='edit_item'),
+    path('remove_item/<int:item_id>/', remove_item, name='remove_item'),
     path('get-item/', get_item_json, name='get_item_json'),
-    path('create-ajax/', create_ajax, name='create-ajax')
+    path('create-ajax/', create_ajax, name='create-ajax'),
+    path('create-flutter/', create_item_flutter, name='create_item_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 import datetime
-from django.http import HttpResponseRedirect
+import json
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
@@ -161,3 +162,22 @@ def create_ajax(request):
         return HttpResponse(b"CREATED", status=201)
 
     return HttpResponseNotFound()
+    
+@csrf_exempt
+def create_item_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Item.objects.create(
+            user = request.user,
+            name = data["name"],
+            amount = int(data["amount"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
